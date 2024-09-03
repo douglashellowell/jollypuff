e32746ce67b9be2ba173c0a6a63681bfa8dcbe8a
Douglas Hellowell <53038423+douglashellowell@users.noreply.github.com>
2024-09-03 12:19:49 -0700

    add cors

--- functions/src/functions/HelloWorld.ts
+++ functions/src/functions/HelloWorld.ts
@@ -1,15 +1,32 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from '@azure/functions';
 
-export async function HelloWorld(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+export async function HelloWorld(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+  const name = request.query.get('name') || (await request.text()) || 'world';
+  // Set CORS headers
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  };
 
-    return { body: `Hello, ${name}!` };
-};
+  return {
+    body: `Hello, ${name}!`,
+    headers: headers,
+  };
+}
 
 app.http('HelloWorld', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: HelloWorld
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  handler: HelloWorld,
 });
